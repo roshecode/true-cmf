f83eb503af88e8ad2c312e736db1b59ee4f11799
roman <sheva2012h@gmail.com>
2016-06-09 19:43:47 +0300

    Create smart ajax

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,15 +1,31 @@
 // var $ = document;
 +function($) {
-    let home = 'http://work/avtomagazin.dp.ua';
+    // let home = 'http://work/avtomagazin.dp.ua';
     // let home = 'http://avto.emis.net.ua';
+    let home = window.location.protocol + '//' + window.location.host + '/avtomagazin.dp.ua';
 
     let placeholder_productsCount = $.querySelector('.block__cart > .field > p');
     let placeholder_totalCost = $.querySelector('.block__cart > .field > .total-cost');
+    let placeholder_content = $.querySelector('.block__content');
     let buttons_addToCart = $.querySelectorAll('.field_add-button');
     let button_showCart = $.querySelector('.block__cart');
-    let block_content = $.querySelector('.block__content');
     let button_search = $.querySelector('.block__search > input[type=submit]');
     let value_search = $.querySelector('.block__search > input[type=search]');
+
+    let lastUrl = window.location.pathname;
+    let skip = false;
+    var changeUrlListener = function() {
+        let url = window.location.pathname;
+        // console.log('CURR URL: ' + url);
+        if (url != lastUrl && !t.ajax.skip) {
+            lastUrl = url;
+            t.ajax.skip = true;
+            console.log('changed');
+            // console.log('LAST URL: ' + lastUrl);
+            content.views[content.data.view]();
+        }
+    };
+    setInterval(changeUrlListener, 1000);
 
     var t = {};
     t.ajax = function(params) {
@@ -17,6 +33,11 @@
         let type = params.type || 'GET',
             dataType = params.dataType || 'html',
             async = params.async || true,
+            success = params.success || function(msg) {
+                    document.open();
+                    document.write(msg);
+                    document.close();
+                },
             request = new XMLHttpRequest();
         request.open(type, params.url + ((type === 'GET' && params.data) ? params.data : ''), async);
 
@@ -27,28 +48,27 @@
                     switch (dataType.toLowerCase()) {
                         case 'json': msg = JSON.parse(msg); break;
                     }
-                    params.success(msg);
+                    success(msg);
                 } else {
                     alert('Ajax false!');
                 }
             }
         };
 
-        // request.send(data.send); // POST
-        // request.send(); // GET
         (type === 'POST' && params.data) ? request.send(params.data) : request.send();
         request = null;
     };
 
-    t.fullAjax = function(params, full) {
-        // let newParams = Object.assign(params, {
-        //     url: params.url + '&'
-        // });
-        // params.part = params.part || true;
-        // params.url = params.url + params.part ? '&part=1' : '';
-        full = full || false;
-        params.url = params.url + (full ? '' : '?part=1');
+    t.smartAjax = function(params, skip = true) {
+        history.pushState({}, '', params.url);
+        t.ajax.skip = skip;
+        params.url = params.url + '?part=1';
         t.ajax(params);
+    };
+
+    t.setUrl = function(view, data) {
+        content.data.view = view || home;
+        history.pushState({}, '', home + '/' + view + (data ? '/' + data : ''));
     };
 
     t.replaceScript = function (oldSrc, newSrc) {
@@ -63,16 +83,31 @@
         t.replaceScript(src, src);
     };
 
-    let content = {};
-    content.showCart = function (full) {
-        t.fullAjax({
-            type: 'GET',
-            url: home + '/cart',
-            success: function (msg) {
-                // $.querySelector('body').insertAdjacentHTML('afterbegin', e.responseText);
-                block_content.innerHTML = msg;
-            }
-        }, full);
+    // This object contains content showing functions
+    let content = { data: {} };
+    content.views = {
+        home: function (full) {
+            alert('home');
+        },
+        cart: function (full) {
+            t.smartAjax({
+                type: 'GET',
+                url: home + '/cart',
+                success: function (msg) {
+                    placeholder_content.innerHTML = msg;
+                }
+            });
+        },
+        search: function (full) {
+            t.smartAjax({
+                type: 'GET',
+                url: home + '/search/' + value_search.value,
+                success: function(msg) {
+                    placeholder_content.innerHTML = msg;
+                    t.updateScript(home + '/frontend/build/js/main.js');
+                }
+            })
+        }
     };
 
     for (let i = 0; i < buttons_addToCart.length; ++i) {
@@ -92,21 +127,18 @@
     }
 
     button_showCart.addEventListener('click', function(e) {
-        content.showCart();
+        
+        content.views.cart();
+        // t.setUrl('cart');
     });
 
     button_search.addEventListener('click', function(e) {
+        // skip = true;
         e.preventDefault();
-        history.pushState({}, 'TEST', home + '/search/' + value_search.value);
-        t.ajax({
-            type: 'GET',
-            url: home + '/search/' + value_search.value,
-            success: function(msg) {
-                // $.body.innerHTML = e.responseText;
-                block_content.innerHTML = msg;
-                t.updateScript(home + '/frontend/build/js/main.js');
-            }
-        })
+        // history.pushState({}, 'TEST', home + '/search/' + value_search.value);
+        // content.views.search();
+        // t.setUrl('search', value_search.value);
+        content.views.search();
     });
 
     // let removeProductFromCart
