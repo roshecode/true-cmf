037308737a191bfcd9971ebbda55f691ac61cc2b
ma1oy <sheva2012h@gmail.com>
2017-05-13 11:28:06 +0300

    update routers benchmark

--- public/benchmark.php
+++ public/benchmark.php
@@ -1,13 +1,14 @@
 <?php
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
 error_reporting(E_ALL);
-require __DIR__ . '/../app/vendor/nikic/fast-route/src/bootstrap.php';
+require __DIR__ . '/../vendor/nikic/fast-route/src/bootstrap.php';
 //spl_autoload_register(function ($class) {
 //    require __DIR__ . '/Pux/src/' . strtr($class, '\\', '/') . '.php';
 //});
-$DIR = __DIR__ . '/../app/core/Support/Services/Routing';
-require $DIR . '/TrueRouter/Router.php';
-require $DIR . '/Router.php';
-require $DIR . '/../Http/Response.php';
+$DIR = __DIR__ . '/../core/Services/Routing';
+//require $DIR . '/TrueRouter/Router.php';
+require $DIR . '/Route.php';
 function callback() {}
 /*$options = [
     'dataGenerator' => 'FastRoute\\OldDataGenerator',
@@ -16,19 +17,21 @@ function callback() {}
 $options = [];
 $nRoutes = 200;
 $nMatches = 30000;
+$lastStr = null;
+
 
 // MY ROUTER ----------------------------------------------------------
 
 //$router = new \Truth\Support\Services\Routing\TrueRouter\Router('');
-$router = new \T\Support\Services\Routing\Router();
+$myrouter = new \T\Services\Routing\Route();
     for ($i = 0, $str = 'a'; $i < $nRoutes; $i++, $str++) {
-        $router->match('GET', '' . $str . '/:arg', 'handler' . $i);
+        $myrouter->match('GET', '' . $str . '/:arg', 'handler' . $i);
         $lastStr = $str;
     }
 // first route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->make('GET', 'a/foo');
+    $res = $myrouter->make('GET', 'a/foo');
 }
 printf("TrueRoute first route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
@@ -36,7 +39,7 @@ printf("TrueRoute first route: %f\n", microtime(true) - $startTime);
 // last route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->make('GET', '' . $lastStr . '/foo');
+    $res = $myrouter->make('GET', '' . $lastStr . '/foo');
 }
 printf("TrueRoute last route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
@@ -44,7 +47,7 @@ printf("TrueRoute last route: %f\n", microtime(true) - $startTime);
 // unknown route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->make('GET', 'foobar/bar');
+    $res = $myrouter->make('GET', 'foobar/bar');
 }
 printf("TrueRoute unknown route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
@@ -55,7 +58,7 @@ echo '<br />';
 
 // FAST ROUTER --------------------------------------------------------
 
-$router = FastRoute\simpleDispatcher(function($router) use ($nRoutes, &$lastStr) {
+$fastrouter = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $router) use ($nRoutes, &$lastStr) {
     for ($i = 0, $str = 'a'; $i < $nRoutes; $i++, $str++) {
         $router->addRoute('GET', '/' . $str . '/{arg}', 'handler' . $i);
         $lastStr = $str;
@@ -64,7 +67,7 @@ $router = FastRoute\simpleDispatcher(function($router) use ($nRoutes, &$lastStr)
 // first route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->dispatch('GET', '/a/foo');
+    $res = $fastrouter->dispatch('GET', '/a/foo');
 }
 printf("FastRoute first route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
@@ -72,7 +75,7 @@ printf("FastRoute first route: %f\n", microtime(true) - $startTime);
 // last route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->dispatch('GET', '/' . $lastStr . '/foo');
+    $res = $fastrouter->dispatch('GET', '/' . $lastStr . '/foo');
 }
 printf("FastRoute last route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
@@ -80,7 +83,7 @@ printf("FastRoute last route: %f\n", microtime(true) - $startTime);
 // unknown route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->dispatch('GET', '/foobar/bar');
+    $res = $fastrouter->dispatch('GET', '/foobar/bar');
 }
 printf("FastRoute unknown route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
@@ -97,29 +100,29 @@ $nMatches = 20000;
 $args = implode('/', array_map(function($i) { return ':arg' . $i; }, range(1, $nArgs)));
 
 //$router = new \Truth\Support\Services\Routing\TrueRouter\Router('');
-$router = new \T\Support\Services\Routing\Router();
+$myrouter = new \T\Services\Routing\Route();
     for ($i = 0, $str = 'a'; $i < $nRoutes; $i++, $str++) {
-        $router->match('GET', '' . $str . '/' . $args, 'handler' . $i);
+        $myrouter->match('GET', '' . $str . '/' . $args, 'handler' . $i);
         $lastStr = $str;
     }
 // first route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->make('GET', 'a/' . $args);
+    $res = $myrouter->make('GET', 'a/' . $args);
 }
 printf("TrueRoute first route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
 // last route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->make('GET', '' . $lastStr . '/' . $args);
+    $res = $myrouter->make('GET', '' . $lastStr . '/' . $args);
 }
 printf("TrueRoute last route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
 // unknown route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->make('GET', 'foobar/' . $args);
+    $res = $myrouter->make('GET', 'foobar/' . $args);
 }
 printf("TrueRoute unknown route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
@@ -133,7 +136,7 @@ echo '<br />';
 //$nMatches = 20000;
 $args = implode('/', array_map(function($i) { return "{arg$i}"; }, range(1, $nArgs)));
 //$args = implode('/', array_map(function($i) { return ':arg' . $i; }, range(1, $nArgs)));
-$router = FastRoute\simpleDispatcher(function($router) use($nRoutes, $args, &$lastStr) {
+$fastrouter = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $router) use($nRoutes, $args, &$lastStr) {
     for ($i = 0, $str = 'a'; $i < $nRoutes; $i++, $str++) {
         $router->addRoute('GET', '/' . $str . '/' . $args, 'handler' . $i);
         $lastStr = $str;
@@ -142,7 +145,7 @@ $router = FastRoute\simpleDispatcher(function($router) use($nRoutes, $args, &$la
 // first route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->dispatch('GET', '/a/' . $args);
+    $res = $fastrouter->dispatch('GET', '/a/' . $args);
 }
 printf("FastRoute first route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
@@ -150,14 +153,14 @@ printf("FastRoute first route: %f\n", microtime(true) - $startTime);
 
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->dispatch('GET', '/' . $lastStr . '/' . $args);
+    $res = $fastrouter->dispatch('GET', '/' . $lastStr . '/' . $args);
 }
 printf("FastRoute last route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
 // unknown route
 $startTime = microtime(true);
 for ($i = 0; $i < $nMatches; $i++) {
-    $res = $router->dispatch('GET', '/foobar/' . $args);
+    $res = $fastrouter->dispatch('GET', '/foobar/' . $args);
 }
 printf("FastRoute unknown route: %f\n", microtime(true) - $startTime);
 //var_dump($res);
